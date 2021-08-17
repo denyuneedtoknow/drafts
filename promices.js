cf7ccef1e3d29daf7df39448e6dc8bbdb023b971
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2021-08-17 22:07:31 +0300

    ajax add

--- promices.js
+++ promices.js
@@ -19,19 +19,19 @@
 // };
 // toggleUserState(users, 'John').then(result => console.log(result));
 
-function createAsyncArray(count) {
-  return new Promise((resolve, reject) => {
-    let arr = [];
-    for (let i = 0; i < count; i++) {
-      arr.push(i);
-    }
-    if (arr.length > 10) {
-      resolve(arr.length);
-    } else {
-      reject('error from promise');
-    }
-  });
-}
-Promise.allSettled([createAsyncArray(1000), createAsyncArray(5), createAsyncArray(10000)])
-  .then(result => console.log(result))
-  .catch(err => console.log('error', err));
+// function createAsyncArray(count) {
+//   return new Promise((resolve, reject) => {
+//     let arr = [];
+//     for (let i = 0; i < count; i++) {
+//       arr.push(i);
+//     }
+//     if (arr.length > 10) {
+//       resolve(arr.length);
+//     } else {
+//       reject('error from promise');
+//     }
+//   });
+// }
+// Promise.allSettled([createAsyncArray(1000), createAsyncArray(5), createAsyncArray(10000)])
+//   .then(result => console.log(result))
+//   .catch(err => console.log('error', err));
