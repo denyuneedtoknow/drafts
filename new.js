f2beb0d136a46ee3331c9b77748591f94aace42f
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2022-01-25 17:00:19 +0200

    Update new.js

--- new.js
+++ new.js
@@ -1,11 +1,12 @@
 function slugify(title) {
     // Change code below this line
-    const normal = title.normalize()
+    const normal = title.toLowerCase().split(" ").join("-")
     console.log(normal);
+    return normal
     const slug = normal.join("-")
     return slug
 
 
     // Change code above this line
 }
-console.log(slugify("Arrays for begginers"));
+slugify("Arrays for begginers");
