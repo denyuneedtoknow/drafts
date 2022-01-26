ee1fa5244ba5519ed1b4b898a656bc54e0ea40f7
Denys Hruzdiev <denyuneedtoknow@gmail.com>
2022-01-26 17:00:51 +0200

    new

--- new.js
+++ new.js
@@ -1,12 +1,57 @@
-function slugify(title) {
-    // Change code below this line
-    const normal = title.toLowerCase().split(" ").join("-")
-    console.log(normal);
-    return normal
-    const slug = normal.join("-")
-    return slug
+// function makeArray(firstArray, secondArray, maxLength) {
+//     // Change code below this line
+//     const newArray = firstArray.concat(secondArray)
+//     if (newArray.length > maxLength) {
+//         console.log(maxLength);
+//         return newArray.slice(0, maxLength)
+//     }
+//     return newArray
+//     // Change code above this line
+// }
+// console.log(makeArray(["Mango"], ["Ajax", "Chelsea", "Poly", "Houston"], 6));
 
 
-    // Change code above this line
+// function calculateTotal(number) {
+//     // Change code below this line
+//     let result = 0
+//     for (let i = 1; i <= number; i++) {
+//         result += i
+//         console.log(result);
+//     }
+
+//     // Change code above this line
+// }
+// calculateTotal(5)
+
+
+// function calculateTotalPrice(order) {
+//     let total = 0;
+//     // Change code below this line
+//     for (let i = 0; i < order.length; i += 1) {
+//         total += order[i]
+//     }
+//     // Change code above this line
+//     console.log(total);
+//     return total;
+// }
+// calculateTotalPrice([412, 371, 94, 63, 176])
+
+function findLongestWord(string) {
+    let biggestWord = ''
+    array = string.split(' ')
+    let wordLength = 0
+    let biggestWordLength = 0
+    for (let i = 0; i < array.length; i += 1) {
+        wordLength = array[i].split('').length
+        if (biggestWordLength <= wordLength) {
+            biggestWordLength = wordLength
+            biggestWord = array[i]
+        }
+    }
+    console.log(biggestWord);
+    return biggestWord
 }
-slugify("Arrays for begginers");
+
+
+
+findLongestWord("May the force 88888888 be with you")
